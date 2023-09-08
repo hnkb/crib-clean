af4b3ca2e7efb112d5d6f4406bce8f4c82539a89
Hani Kaabi <hani@hnkb.de>
2023-09-09 01:16:44 +0200

    -- test move

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,28 @@
 #include <crib/app>
 
 
+class myWindow : public crib::app::window
+{
+public:
+	void on_size_changed(int2 dims) override
+	{
+		window::on_size_changed(dims);
+		printf("size\n");
+	}
+	void on_position_changed(int2 pos) override
+	{
+		window::on_position_changed(pos);
+		printf("move\n");
+	}
+};
+
 int main()
 {
 	try
 	{
-		crib::app::window win;
+		//crib::app::window win;
+		myWindow win2;
+		auto win = std::move(win2);
 		return crib::app::run();
 	}
 	catch (std::exception& ex)
