88cc925257da89d2f7b929cc10ce7229a451798d
Hani Kaabi <hani@hnkb.de>
2023-09-09 02:34:50 +0200

    Quick & dirty triangle program

--- src/graphics/gl/context_gl.cpp
+++ src/graphics/gl/context_gl.cpp
@@ -1,8 +1,138 @@
 
 #include "context_gl.h"
 #include <glad/glad.h>
+#include <algorithm>
+#include <vector>
 
 using crib::graphics::gl::context;
+
+
+GLuint theProgram = 0;
+
+
+GLuint CreateShader(GLenum eShaderType, const std::string& strShaderFile)
+{
+	GLuint shader = glCreateShader(eShaderType);
+	const char* strFileData = strShaderFile.c_str();
+	glShaderSource(shader, 1, &strFileData, NULL);
+
+	glCompileShader(shader);
+
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+
+		const char* strShaderType = NULL;
+		switch (eShaderType)
+		{
+			case GL_VERTEX_SHADER:
+				strShaderType = "vertex";
+				break;
+			case GL_GEOMETRY_SHADER:
+				strShaderType = "geometry";
+				break;
+			case GL_FRAGMENT_SHADER:
+				strShaderType = "fragment";
+				break;
+		}
+
+		fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
+		delete[] strInfoLog;
+	}
+
+	return shader;
+}
+
+GLuint CreateProgram(const std::vector<GLuint>& shaderList)
+{
+	GLuint program = glCreateProgram();
+
+	for (size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+		glAttachShader(program, shaderList[iLoop]);
+
+	glLinkProgram(program);
+
+	GLint status;
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+		delete[] strInfoLog;
+	}
+
+	for (size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+		glDetachShader(program, shaderList[iLoop]);
+
+	return program;
+}
+
+
+
+const std::string strVertexShader(
+	"#version 330 core\n"
+	"layout(location = 0) in vec4 position;"
+	"void main()"
+	"{"
+	"   gl_Position = position;"
+	"}");
+
+const std::string strFragmentShader(
+	"#version 330 core\n"
+	"out vec4 outputColor;"
+	"void main()"
+	"{"
+	"   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);"
+	"}");
+
+void InitializeProgram()
+{
+	std::vector<GLuint> shaderList;
+
+	shaderList.push_back(CreateShader(GL_VERTEX_SHADER, strVertexShader));
+	shaderList.push_back(CreateShader(GL_FRAGMENT_SHADER, strFragmentShader));
+
+	theProgram = CreateProgram(shaderList);
+
+	std::for_each(shaderList.begin(), shaderList.end(), glDeleteShader);
+}
+
+const float vertexPositions[] = {
+	0.75f, 0.75f, 0.0f, 1.0f, 0.75f, -0.75f, 0.0f, 1.0f, -0.75f, -0.75f, 0.0f, 1.0f,
+};
+
+GLuint positionBufferObject;
+GLuint vao;
+
+
+void InitializeVertexBuffer()
+{
+	glGenBuffers(1, &positionBufferObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+
+void initGL_3()
+{
+	InitializeProgram();
+	InitializeVertexBuffer();
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+}
 
 
 void context::on_resize(int2 dims)
@@ -39,4 +169,18 @@ void context::draw_platform_independent()
 {
 	glClearColor(0.6f, 0.2f, 0.15f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	if (theProgram == 0)
+		initGL_3();
+
+	glUseProgram(theProgram);
+
+	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(0);
+	glUseProgram(0);
 }
